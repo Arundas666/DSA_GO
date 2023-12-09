733920b85f3541e5d24f1389eb3d6a1250d03200
Arundas666 <arundas285@gmail.com>
2023-12-09 19:07:54 +0530

    merge and quick sort

---
+++ Sort/Merge Sort/Dsc/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	var arr = []int{2, 4, 1, 6, 3, 8, 2}
+	fmt.Println(mergeSort(arr))
+
+}
+func mergeSort(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+	mid := len(arr) / 2
+	left := mergeSort(arr[:mid])
+	right := mergeSort(arr[mid:])
+	return merge(left, right)
+}
+func merge(left, right []int) []int {
+	result := make([]int, 0, len(left)+len(right))
+	i, j := 0, 0
+	for i < len(left) && j < len(right) {
+		if left[i] > right[j] {
+			result = append(result, left[i])
+			i++
+		} else {
+			result = append(result, right[j])
+			j++
+		}
+	}
+	result = append(result, left[i:]...)
+	result = append(result, right[j:]...)
+	return result
+}
